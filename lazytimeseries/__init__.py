72f4c60bead7ae2d9d12d211469f4e00240c5684
ljwoods2 <ljwoods2@asu.edu>
2024-06-25 09:49:59 -0700

    Initial creation with MDAnalysis Cookiecutter 0.1

---
+++ lazytimeseries/__init__.py
@@ -0,0 +1,9 @@
+"""
+lazytimeseries
+Hackathon code for a lazy timeseries and analysis code
+"""
+
+# Add imports here
+from importlib.metadata import version
+
+__version__ = version("lazytimeseries")
