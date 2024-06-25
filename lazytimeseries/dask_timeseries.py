79b2eb74442663137e00439d986fdb71abedbd06
ljwoods2 <ljwoods2@asu.edu>
2024-06-25 15:59:47 -0700

    upload

--- lazytimeseries/dask_timeseries.py
+++ lazytimeseries/dask_timeseries.py
@@ -1,58 +1,73 @@
 import MDAnalysis as mda
-from MDAnalysis.analysis import align
+from MDAnalysis.analysis import align, rms
 import zarr
 import dask.array as da
+from MDAnalysisTests.datafiles import PSF, DCD
+import numpy as np
+import time
+
 
 def align_trajectory(u, outputfile):
-    average = align.AverageStructure(u, u, select='protein and name CA',
-                                 ref_frame=0).run()
+    average = align.AverageStructure(
+        u, u, select="protein and name CA", ref_frame=0
+    ).run()
     ref = average.results.universe
-    aligner = align.AlignTraj(u, ref,
-                           select='protein and name CA',
-                           filename=outputfile,
-                           in_memory=False).run()
+    aligner = align.AlignTraj(
+        u,
+        ref,
+        select="protein and name CA",
+        filename=outputfile,
+        in_memory=False,
+    ).run()
+
 
 def convert_universe_to_pos_timeseries(u, zarr_filename):
     shape = (u.trajectory.n_frames, u.atoms.n_atoms, 3)
-    z = zarr.open(zarr_filename, mode='w', shape=shape, dtype='f4')
+    z = zarr.open(zarr_filename, mode="w", shape=shape, dtype="f4")
     for i in range(len(u.trajectory)):
         z[i] = u.trajectory[i].positions
-        
+
+
 def get_dask_timeseries_from(zarr_filename):
     z = zarr.open(zarr_filename)
     return da.from_zarr(z)
 
-def calculate_rmsf(positions):
-    """
-    Calculate the Root Mean Squared Fluctuation (RMSF) for each atom given a dask array of positions.
-    
-    Parameters:
-        positions (dask.array.Array): A Dask array of shape (n_frames, n_atoms, n_dimensions),
-                                      where n_frames is the number of frames, n_atoms is the number
-                                      of atoms, and n_dimensions is the spatial dimensions (e.g., 3 for x, y, z).
-    
-    Returns:
-        dask.array.Array: A Dask array of shape (n_atoms,) containing the RMSF for each atom.
-    """
-    # Compute the mean positions of each atom across all frames
+
+def calculate_rmsf_displace(positions):
+    # scalar_dispacement = da.sqrt((positions**2).sum(axis=2))
     mean_positions = positions.mean(axis=0)
-    
-    # Compute the squared deviations from the mean for each frame
-    squared_deviations = (positions - mean_positions) ** 2
-    
-    # Compute the mean squared deviations for each atom
-    mean_squared_deviations = squared_deviations.mean(axis=0)
-    
-    # Compute the RMSF by taking the square root of the mean squared deviations
-    rmsf = da.sqrt(mean_squared_deviations.mean(axis=1))
-    
-    return rmsf
+    subtracted_positions = positions - mean_positions
+    squared_deviations = subtracted_positions**2
+    avg_squared_deviations = squared_deviations.mean(axis=0)
+    sqrt_avg_squared_deviations = da.sqrt(avg_squared_deviations)
+    return da.sqrt((sqrt_avg_squared_deviations**2).sum(axis=1))
+
 
 if __name__ == "__main__":
-    #u = mda.Universe("yiip_equilibrium/YiiP_system.pdb", "yiip_equilibrium/YiiP_system_90ns_center.xtc")
-    #align_trajectory(u, 'yiip_positions_aligned.xtc')
-    u_aligned = mda.Universe("yiip_equilibrium/YiiP_system.pdb", "yiip_positions_aligned.xtc")
-    convert_universe_to_pos_timeseries(u_aligned, 'yiip_positions_aligned.zarr')
-    dask_timeseries = get_dask_timeseries_from('yiip_positions_aligned.zarr')
-    rmsf = calculate_rmsf(dask_timeseries)
-    rmsf.compute() 
+    # u = mda.Universe(PSF, DCD)
+    # align_trajectory(u, "positions_aligned.dcd")
+    u_aligned = mda.Universe(
+        "yiip_equilibrium/YiiP_system.pdb",
+        "yiip_positions_aligned.xtc",
+    )
+    # convert_universe_to_pos_timeseries(u_aligned, "traj_aligned.zarr")
+
+    dask_timeseries = get_dask_timeseries_from("yiip_positions.zarr")
+    np.testing.assert_allclose(
+        dask_timeseries[:], u_aligned.trajectory.timeseries(order="fac")
+    )
+
+    myt1 = time.time()
+    rmsf_last = calculate_rmsf_displace(dask_timeseries)
+    myresult2 = rmsf_last.compute()
+    myt2 = time.time()
+
+    mdt1 = time.time()
+    R = rms.RMSF(u_aligned.atoms).run()
+    mdt2 = time.time()
+
+    print(f"Dask RMSF time was: {myt2 - myt1}")
+    print(f"MDAnalysis RMSF time was: {mdt2 - mdt1}")
+
+    np.testing.assert_allclose(R.results.rmsf, myresult2)
+    # Good enough!
